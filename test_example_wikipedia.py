b9b084f05dd0fbcdeca04686b81bb9ec3833beeb
Donny Navarro <donaldjnavarro@gmail.com>
2025-05-27 14:23:30 -0400

    Created basic example UI tests

---
+++ test_example_wikipedia.py
@@ -0,0 +1,32 @@
+"""Example snippets for Python automated testing using Wikipedia as a test subject"""
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+def test_example_wikipedia():
+    """Example Selenium test: Wikipedia"""
+
+    # Navigate to Wikipedia
+    driver = webdriver.Chrome()
+    driver.get("https://www.wikipedia.com")
+    
+    # Confirm the page loaded
+    landing_title = "Wikipedia"
+    assert landing_title in driver.title, "Expected '{}' to be in the page title, but received: '{}'".format(landing_title, driver.title)
+
+    # Take an action on the page
+    input_text = "Selenium UI automation"
+    search_box = driver.find_element(By.NAME, "search")
+    search_box.send_keys(input_text)
+    search_box.submit()
+    time.sleep(2)
+
+    # Confirm the results of the action
+    search_results_title = "{} - Search results".format(input_text)
+    assert search_results_title in driver.title, "Expected '{}' to be in the page title, but received: '{}'".format(search_results_title, driver.title)
+
+    # Clean up
+    driver.quit()
+
+if __name__ == "__main__":
+        test_example_wikipedia()
